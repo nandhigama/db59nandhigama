1403abdb08260587c144af1e38025a464c472279
Nandhigama <n.prasannareddy1398@gmail.com>
2021-11-10 23:04:44 -0600

    Added file

--- routes/bat.js
+++ routes/bat.js
@@ -1,5 +1,10 @@
 var express = require('express');
+const bat_controlers= require('../controllers/bat');
 var router = express.Router();
+/* GET bottle */
+
+router.get('/', bat_controlers.bat_view_all_Page );
+module.exports = router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
