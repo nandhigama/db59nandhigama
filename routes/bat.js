56a08097a268779d1b7f5dd0f877f5223448d6d1
Nandhigama <n.prasannareddy1398@gmail.com>
2021-11-17 17:24:00 -0600

    Added file

--- routes/bat.js
+++ routes/bat.js
@@ -14,3 +14,12 @@ router.get('/', function(req, res, next) {
 module.exports = router;
 // GET request for one bat.
 router.get('/bat/:id', bat_controlers.bat_detail);
+
+/* GET detail bat page */
+router.get('/detail', bat_controlers.bat_view_one_Page);
+/* GET create bat page */
+router.get('/create', bat_controlers.bat_create_Page);
+/* GET create update page */
+router.get('/update', bat_controlers.bat_update_Page);
+/* GET create bat page */
+router.get('/delete', bat_controlers.bat_delete_Page);
