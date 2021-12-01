2bfa8f3751c72abbdf7e7d8a5b0aa243bcf63066
Nandhigama <n.prasannareddy1398@gmail.com>
2021-11-30 19:15:21 -0600

    added bonus

--- routes/bat.js
+++ routes/bat.js
@@ -23,10 +23,10 @@ module.exports = router;
 router.get('/bat/:id', bat_controlers.bat_detail);
 
 /* GET detail bat page */
-router.get('/detail', bat_controlers.bat_view_one_Page);
+router.get('/detail',secured, bat_controlers.bat_view_one_Page);
 /* GET create bat page */
 router.get('/create', bat_controlers.bat_create_Page);
 /* GET create update page */
 router.get('/update',secured, bat_controlers.bat_update_Page);
 /* GET create bat page */
-router.get('/delete', bat_controlers.bat_delete_Page);
+router.get('/delete',secured, bat_controlers.bat_delete_Page);
