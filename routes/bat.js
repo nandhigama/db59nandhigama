4d4999fdcb9ba46d063e9a77399940b95c57db39
Nandhigama <n.prasannareddy1398@gmail.com>
2021-11-30 19:10:54 -0600

    added upto part 5

--- routes/bat.js
+++ routes/bat.js
@@ -2,6 +2,13 @@ var express = require('express');
 const bat_controlers= require('../controllers/bat');
 var router = express.Router();
 /* GET bottle */
+const secured = (req, res, next) => { 
+  if (req.user){ 
+    return next(); 
+  } 
+  req.session.returnTo = req.originalUrl; 
+  res.redirect("/login"); 
+} 
 
 router.get('/', bat_controlers.bat_view_all_Page );
 module.exports = router;
@@ -20,6 +27,6 @@ router.get('/detail', bat_controlers.bat_view_one_Page);
 /* GET create bat page */
 router.get('/create', bat_controlers.bat_create_Page);
 /* GET create update page */
-router.get('/update', bat_controlers.bat_update_Page);
+router.get('/update',secured, bat_controlers.bat_update_Page);
 /* GET create bat page */
 router.get('/delete', bat_controlers.bat_delete_Page);
